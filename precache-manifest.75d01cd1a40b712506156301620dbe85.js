84ee22a1a221883a4c45778ccd2d089d3a3186d5
LeYuliia <legeza.y@gmail.com>
2020-07-07 16:06:39 +0300

    Updates

---
+++ precache-manifest.75d01cd1a40b712506156301620dbe85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d9650d26cd93049ca1f322e40f2e85",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "b68ae786f9ec3eeedf35",
+    "url": "/goit-react-hw-03-phonebook/static/css/2.34ca2c66.chunk.css"
+  },
+  {
+    "revision": "0ae5881f9efa9da3da24",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.409a6797.chunk.css"
+  },
+  {
+    "revision": "b68ae786f9ec3eeedf35",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.27f100ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.27f100ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ae5881f9efa9da3da24",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.77b972b3.chunk.js"
+  },
+  {
+    "revision": "1d2b87b5e2a4d49e8d82",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.78e50734.js"
+  }
+]);
